675d026c47a262c0b63d410bc0028a70f0f39f75
jorgewu <jcwuzhang@gmail.com>
2020-01-06 18:11:22 +0100

    Added dropdown buttons styling | Debug

---
+++ JavaScript/test.js
@@ -0,0 +1,18 @@
+document.body.onload = function() {
+  chrome.storage.sync.get("data", function(items) {
+    if (!chrome.runtime.error) {
+      console.log(items);
+      document.getElementById("data").value = items.data;
+    }
+  });
+}
+
+document.getElementById("set").onclick = function() {
+  var d = document.getElementById("text").value;
+  chrome.storage.sync.set({ "data" : d }, function() {
+    if (chrome.runtime.error) {
+      console.log("Runtime error.");
+    }
+  });
+  window.close();
+}
