235f63967305a376a2741de16a50fc21db62820f
jorgewu <jcwuzhang@gmail.com>
2020-01-06 21:25:28 +0100

    Added JS comments on new_popup.js

--- JavaScript/new_popup.js
+++ JavaScript/new_popup.js
@@ -1,5 +1,6 @@
-document.getElementById("settings").addEventListener("click", openSettings);
+document.getElementById("settings").addEventListener("click", openSettings); //This code will grab the settings button and add an event listener
 
+//function that settings button will trigger when a click is registered. The function creates a new popup with the specified measures
 function openSettings() {
   chrome.windows.create({'url': 'options.html','type': 'popup', 'width': 400, 'height': 600});
 }
