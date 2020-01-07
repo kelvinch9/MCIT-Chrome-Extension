8079e077f5f3642c6aca9a95dd5501685401d7df
kelvinch9 <kelvinch@seas.upenn.edu>
2020-01-07 02:29:47 -0500

    +load feature in chrome.storage.sync

--- JavaScript/loadUser.js
+++ JavaScript/loadUser.js
@@ -2,8 +2,17 @@
 
 //failing because of asynchronous issue. see: https://stackoverflow.com/questions/18699075/callback-returns-undefined-with-chrome-storage-sync-get
 
-chrome.storage.sync.get(['first', 'email'], function(result){
-  document.getElementById("name").innerHTML = result.key;
-  document.getElementById("email").innerHTML = result.key;
+document.body.onload = function() {
+  chrome.storage.sync.get(["first", "last", "email"], function(items) {
+    if (!chrome.runtime.error) {
+      console.log(items);
+      document.getElementById("name").innerText = items.first + " " + items.last;
+      document.getElementById("email").innerText = items.email;
+    }
+  });
+}
 
-});
+// chrome.storage.sync.get(['first', 'email'], function(result){
+//   document.getElementById("name").innerHTML = result.key;
+//   document.getElementById("email").innerHTML = result.key;
+// });
