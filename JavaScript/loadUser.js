67eba79c829890d5db0a82d61f883124412cda15
kelvinch9 <kelvinch@seas.upenn.edu>
2020-01-07 21:43:23 -0500

    options page now remember's user choices

--- JavaScript/loadUser.js
+++ JavaScript/loadUser.js
@@ -1,18 +1,12 @@
 //load the user's first, last, pennID, email from chrome.storage
 
-//failing because of asynchronous issue. see: https://stackoverflow.com/questions/18699075/callback-returns-undefined-with-chrome-storage-sync-get
-
 document.body.onload = function() {
-  chrome.storage.sync.get(["first", "last", "email"], function(items) {
+  chrome.storage.sync.get(["first", "last", "email", "pennID"], function(items) {
     if (!chrome.runtime.error) {
       console.log(items);
       document.getElementById("name").innerText = items.first + " " + items.last;
       document.getElementById("email").innerText = items.email;
+      document.getElementById("pennID").innerText = "Penn ID: " + items.pennID;
     }
   });
 }
-
-// chrome.storage.sync.get(['first', 'email'], function(result){
-//   document.getElementById("name").innerHTML = result.key;
-//   document.getElementById("email").innerHTML = result.key;
-// });
--- JavaScript/options.js
+++ JavaScript/options.js
@@ -1,14 +1,37 @@
 // Saves options to chrome.storage
 
 document.getElementById("save").onclick = function() {
+  // save user identity
   var first = document.getElementById("first").value;
   var last = document.getElementById("last").value;
   var email = document.getElementById("email").value;
   var pennID = document.getElementById("pennID").value;
-  chrome.storage.sync.set({"first":first, "last":last, "email":email, "pennID":pennID}, function() {
+  //save courses
+  var cit591 = document.getElementById("cit591").checked;
+  chrome.storage.sync.set({"first":first,
+                            "last":last,
+                            "email":email,
+                            "pennID":pennID,
+                            "cit591":cit591}, function() {
     if (chrome.runtime.error) {
       console.log("Runtime error.");
     }
   });
   window.close();
+}
+
+// loads saved options onto the options page
+document.body.onload = function() {
+  chrome.storage.sync.get(["first", "last", "email", "pennID"], function(items) {
+    if (!chrome.runtime.error) {
+      console.log(items);
+      // load user identity
+      document.getElementById("first").value = items.first;
+      document.getElementById("last").value = items.last;
+      document.getElementById("email").value = items.email;
+      document.getElementById("pennID").value = items.pennID;
+      // load courses
+      document.getElementById("cit591").checked = items.cit591; // unable to load checkbox / courses
+      }
+  });
 }
